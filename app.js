2fc57cc517e43cc3d7231552181300d459595e5c
marialicas <eialicag@gmail.com>
2025-03-05 20:22:23 -0300

    finish projeo

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 let listaAmigos = [];
+let botao = document.querySelector(".button-draw");
+botao.style.backgroundColor = "#444444";
+
+function enter(evento){
+  if(evento.keyCode == 13){
+    adicionarAmigo();
+    limparInput();
+  }
+}
 
 function adicionarAmigo() {
   let amigo = document.querySelector("input").value;
+  
   if (amigo == "") {
     alert("O campo está vazio");
   } else {
+    botao.style.backgroundColor = "#fe652b";
+    botao.toggleAttribute("disabled",false);
     listaAmigos.push(amigo);
     listarAmigos(amigo);
   }
@@ -16,4 +28,25 @@ function listarAmigos(amigo) {
 
   elementoFilho.innerHTML = amigo;
   elementoPai.appendChild(elementoFilho);
+}
+
+function sortearAmigo() {
+  let qtdeAmigos = listaAmigos.length;
+  let amigoSorteado = parseInt(Math.random() * qtdeAmigos);
+  let resultado = document.querySelector("#resultado");
+
+  resultado.innerHTML = `O seu amigo secreto é: ${listaAmigos[amigoSorteado]}!!`;
+
+  document.querySelectorAll("ul#listaAmigos li").forEach((elemento) => {
+    elemento.remove();
+  });
+
+  botao.toggleAttribute("disabled");
+  botao.style.backgroundColor = "#444444"
+  listaAmigos = [];
+}
+
+function limparInput(){
+  let input = document.querySelector("#amigo");
+  input.value = "";
 }
